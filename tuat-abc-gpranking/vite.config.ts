d6ff7648858b1539c6589904b4cd237350211b6a
lX57 <36733349+K-Yoshizawa@users.noreply.github.com>
2025-04-06 09:22:45 +0900

    change

---
+++ tuat-abc-gpranking/vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/gpranking/', // リポジトリ名をここに記入
+});
